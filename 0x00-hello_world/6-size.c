b833ac1b4f905ec9163809cc404635fe8a9f3698
Eddie Phiri <eddiephiri44@gmail.com>
2023-03-16 23:08:32 -0700

    Add newlines to all output statements

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,9 +7,9 @@
  */
 int main(void)
 {
-printf("Size of a char: %d byte(s)",sizeof(char));
-printf("Size of an int: %d bytes(s)",sizeof(int));
-printf("Size of a long int: %d byte(s)",sizeof(long));
-printf("Size of a long long int: %d bytes(s)",sizeof(long long));
-printf("Size of a float: %d byte(s)",sizeof(float));
+printf("Size of a char: %d byte(s)\n",sizeof(char));
+printf("Size of an int: %d bytes(s)\n",sizeof(int));
+printf("Size of a long int: %d byte(s)\n",sizeof(long));
+printf("Size of a long long int: %d bytes(s)\n",sizeof(long long));
+printf("Size of a float: %d byte(s)\n",sizeof(float));
 }
