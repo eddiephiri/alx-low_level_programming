7270d6dec96b6af290e6e06a5b837668dc27232d
Eddie Phiri <eddiephiri44@gmail.com>
2023-03-20 14:01:41 -0700

    Fixed undefined counter

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-int counter = 0;
+int i = 0;
 char alphabet[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w',
 'x', 'y', 'z'};
