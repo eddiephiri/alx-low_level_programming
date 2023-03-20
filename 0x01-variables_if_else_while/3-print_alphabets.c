bbd69c6648e8857241640bf779d54610f795809b
Eddie Phiri <eddiephiri44@gmail.com>
2023-03-20 14:39:27 -0700

    Fix whitespace errors

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - Prints the alphabet in lowercase and uppercase characters and a newline.
- * 
+ * main - Prints the alphabet in lowercase and uppercase
+ * characters and a newline.
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
@@ -10,13 +11,13 @@ int main(void)
 int i = 97;
 int j = 65;
 
-while(i <= 122)
-{   
+while (i <= 122)
+{
 putchar("%c", i);
 i++;
 }
 
-while(j <= 90)
+while (j <= 90)
 {
 putchar("%c", j);
 j++;
