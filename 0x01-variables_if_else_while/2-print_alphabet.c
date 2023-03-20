f92e9ad938ba23cf35845518a33cf8dbba5a0dde
Eddie Phiri <eddiephiri44@gmail.com>
2023-03-20 13:58:11 -0700

    Updated code to use while loop

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,13 +7,15 @@
  */
 int main(void)
 {
+int counter = 0;
 char alphabet[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w',
 'x', 'y', 'z'};
 
-for (int i = 0; i < 26; i++)
+while (i < 26)
 {
 putchar(alphabet[i]);
+i++;
 }
 putchar('\n');
 }
