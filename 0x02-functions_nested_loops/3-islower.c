4abf8fd56b5ec156e3fa24ede2dc18f9c2eed880
Eddie Phiri <eddiephiri44@gmail.com>
2023-03-26 16:16:53 -0700

    Add 3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * islower - returns a boolean
+ *@c: Integer value to be checked
+ *
+ * Return: 1 if @c is lowercase, 0 if otherwise.
+ */
+int islower(int c)
+{
+if (c >= 97 && c <= 122)
+{
+return (1);
+}
+return (0);
+}
