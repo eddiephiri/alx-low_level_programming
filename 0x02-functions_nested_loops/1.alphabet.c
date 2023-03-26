e4927537eb3740dc99524af2f4abc5baa4a23c93
Eddie Phiri <eddiephiri44@gmail.com>
2023-03-26 11:57:20 -0700

    Add print_aphabet function

---
+++ 0x02-functions_nested_loops/1.alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Prints the alphabet in lowercase with a newline.
+ *
+ * Return: 0 (Success)
+ */
+void print_alphabet(void)
+{
+int base = 97;
+
+while (base <= 122)
+{
+_putchar(base);
+}
+
+_putchar('\n');
+
+return (0);
+}
