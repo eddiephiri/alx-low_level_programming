1c61f76ea9877c0e2aad047c9c8d17a8c7ebfb24
Eddie Phiri <eddiephiri44@gmail.com>
2023-04-02 04:32:28 -0700

    Add main.h header file to the new directory

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,12 @@
+/**
+ * main.h header file - Includes function prototypes
+ *
+ * Return: Provides function prototypes when included
+ */
+
+int _putchar(char);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
